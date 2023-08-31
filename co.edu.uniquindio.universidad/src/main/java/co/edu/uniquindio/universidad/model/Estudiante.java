7c5ab6a2c125ddae61affb1f97563572136510f4
Leonardo Gallego Ríos <94010100+k0rz0@users.noreply.github.com>
2023-08-31 15:36:09 -0500

    Clases
    Clase Estudiante
    Clase Docente
    Constructores

--- co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/model/Estudiante.java
+++ co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/model/Estudiante.java
@@ -10,7 +10,20 @@ public class Estudiante {
     private double nota1;
     private double nota2;
 
-    
+    /*Constructor*/
+    public Estudiante(){
+
+    }
+
+    public Estudiante(String nombre, int edad, String correo, int semestre, double nota1, double nota2, double nota3){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+        this.semestre = semestre;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
 
     public int getSemestre() {
         return semestre;
