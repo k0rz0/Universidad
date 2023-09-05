a981cb2cc71d71e6be8ef43b7723e6f237f4281c
Leonardo Gallego Ríos <94010100+k0rz0@users.noreply.github.com>
2023-09-04 22:26:07 -0500

    ultimo de la clase

--- co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/Main.java
+++ co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/Main.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.universidad;
 
+import co.edu.uniquindio.universidad.model.Curso;
 import co.edu.uniquindio.universidad.model.Estudiante;
 import co.edu.uniquindio.universidad.model.Docente;
 
@@ -10,8 +11,16 @@ public class Main {
         Estudiante estudiante2 = crearEstudiantePorConstructor("Pedro", 30, "12121@", 3, 3.0, 4.0, 5.0);
         Estudiante estudiante3 = crearEstudiante("Ana", 19, "12121@", 4, 50, 3.0, 2.0);
 
-        Docente docente = new Docente("Carlos", 30, "sdsd@");
+        Docente docente = new Docente("Jhon Alberth", 47, "JA@mail.co");
 
+        Curso curso = new Curso("Programacion1", 2, "02N", 3, "Nocturna");
+        curso.setEstudiante1(estudiante1);
+        curso.setEstudiante2(estudiante2);
+        curso.setEstudiante3(estudiante3);
+        curso.setDocente(docente);
+
+
+        System.out.println("");
     }
 
     private static Estudiante crearEstudiante(String nombre, int edad, String correo, int semestre, double nota1, double nota2, double nota3) {
